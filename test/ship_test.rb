6f9f2585349d80c26cd8fe6267858828c20c57c5
Drew Williams <drewwilliams5280@gmail.com>
2020-07-07 16:20:27 -0600

    Tests if ship has name and length

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -8,5 +8,13 @@ class ShipTest < Minitest::Test
     assert_instance_of Ship, cruiser
   end
 
-  
+  def test_if_ship_has_name
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal "Cruiser", cruiser.name
+  end
+
+  def test_if_ship_has_length
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal 3, cruiser.length
+  end
 end
