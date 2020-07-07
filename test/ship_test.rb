53a2d5c9eb47c3a428a07181b6a358d79c22e077
Drew Williams <drewwilliams5280@gmail.com>
2020-07-07 12:53:52 -0600

    Tests ship class exists

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -3,5 +3,10 @@ require 'minitest/pride'
 require './lib/ship'
 
 class ShipTest < Minitest::Test
+  def test_it_exists
+    cruiser = Ship.new("Cruiser", 3)
+    assert_instance_of Ship, cruiser
+  end
 
+  
 end
