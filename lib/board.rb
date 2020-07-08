9f74f4822ab626b4c28c6ca1ed192d92b8a93e77
Drew Williams <drewwilliams5280@gmail.com>
2020-07-07 21:38:01 -0600

    Adds valid_coordinate method and test

--- lib/board.rb
+++ lib/board.rb
@@ -27,4 +27,10 @@ class Board
       "D4" => Cell.new("D4"),
     }
   end
+
+  def valid_coordinate?(coordinate)
+    cells.keys.include? coordinate
+  end
 end
+
+require 'pry'; binding.pry
--- test/board_test.rb
+++ test/board_test.rb
@@ -16,6 +16,17 @@ class BoardTest < Minitest::Test
     board.cells
 
     assert_equal 16, board.cells.keys.count
-    assert_equal 16, board.cells.values.count 
+    assert_equal 16, board.cells.values.count
+  end
+
+  def test_if_coordinate_is_valid
+    board = Board.new
+    board.cells
+
+    assert_equal true, board.valid_coordinate?("A1")
+    assert_equal true, board.valid_coordinate?("B2")
+    assert_equal true, board.valid_coordinate?("C3")
+    assert_equal true, board.valid_coordinate?("D4")
+    assert_equal false, board.valid_coordinate?("Z8")
   end
 end
