d476daf563c584ff744cae2bd3a0bc65cfef5b6d
Drew Williams <drewwilliams5280@gmail.com>
2020-07-08 16:22:10 -0600

    valid_placement? method fixed and tests out

--- test/board_test.rb
+++ test/board_test.rb
@@ -49,6 +49,47 @@ class BoardTest < Minitest::Test
     assert board.horizontal_placement?(["A1", "A2", "A3"])
     refute board.horizontal_placement?(["A1", "B1"])
   end
+
+  def test_if_placement_is_vertical
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    refute board.vertical_placement?(["A1", "A2", "A3"])
+    assert board.vertical_placement?(["A1", "B1"])
+  end
+
+  def test_if_coordinates_are_consecutive
+    board = Board.new
+
+    assert board.consecutive_coordinates?(["A1", "A2", "A3"])
+    assert board.consecutive_coordinates?(["C2", "C3", "C4"])
+    assert board.consecutive_coordinates?(["A1", "A2"])
+    assert board.consecutive_coordinates?(["A1", "B1"])
+    assert board.consecutive_coordinates?(["B3", "C3", "D3"])
+    refute board.consecutive_coordinates?(["A1", "A2", "A4"])
+    refute board.consecutive_coordinates?(["A2", "A1"])
+    refute board.consecutive_coordinates?(["B1", "C1", "D2"])
+    refute board.consecutive_coordinates?(["D3", "D1", "D2"])
+  end
+
+  def test_if_placement_is_valid
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    refute board.valid_placement?(cruiser, ["A1", "A2"])
+    refute board.valid_placement?(submarine, ["A2", "A3", "A4"])
+    refute board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+    refute board.valid_placement?(submarine, ["A1", "C1"])
+    refute board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    refute board.valid_placement?(submarine, ["C1", "B1"])
+    refute board.valid_placement?(cruiser, ["A1", "B2", "C3"])
+    refute board.valid_placement?(submarine, ["C2", "D3"])
+    assert board.valid_placement?(submarine, ["A1", "A2"])
+    assert board.valid_placement?(cruiser, ["B1", "C1", "D1"])
+  end
+
 end
 
 # def test_if_coordinates_are_consecutive
