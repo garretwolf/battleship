07603909f22ab029fe951a421966892034ab191c
Garret Wolf <garretwolf26@gmail.com>
2020-07-11 00:35:15 -0400

    Create game file and test and add start method

---
+++ test/game_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game'
+
+class GameTest < Minitest::Test
+  def test_it_exists
+    game = Game.new
+
+    assert_instance_of Game, game
+  end
+end
